6a1c95f00d64581483800a6ce1d4f2f9d5129df8
SantiagoBurzolino <zemgogamer1@gmail.com>
2025-08-13 10:02:37 -0300

    creamos una ruta que recibe un POST

---
+++ servidor/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/api/sensor",methods=["POST"])
+def sensor():
+
+    datos = request.json
+    nombre = datos["nombre"]
+    valor = datos["valor"]
+
+    print (f"sensor: {nombre}, valor: {valor}")
+    
+    return "OK"
