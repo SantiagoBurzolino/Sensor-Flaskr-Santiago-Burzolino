acaf270deb29e2213f9f0c4e257c69bb2c49ab96
SantiagoBurzolino <zemgogamer1@gmail.com>
2025-08-20 09:56:48 -0300

    pusimos el codigo para probar la conexion con la base de datos

--- servidor/app.py
+++ servidor/app.py
@@ -1,6 +1,28 @@
-from flask import Flask, request
+import sqlite3
+from flask import Flask, g, jsonify, request, url_for
+from math import ceil
+
+def dict_factory(cursor, row):
+ """Arma un diccionario con los valores de la fila."""
+ fields = [column[0] for column in cursor.description]
+ return {key: value for key, value in zip(fields, row)}
+
+def abrirConexion():
+  if 'db' not in g:
+     g.db = sqlite3.connect("SQL_Sensor.sqlite")
+     g.db.row_factory = dict_factory
+  return g.db
+
+
+def cerrarConexion(e=None):
+   db = g.pop('db', None)
+   if db is not None:
+       db.close()
+
 
 app = Flask(__name__)
+app.teardown_appcontext(cerrarConexion)
+resultados_por_pag = 10
 
 @app.route("/")
 def hello_world():
@@ -9,11 +31,13 @@ def hello_world():
 
 @app.route("/api/sensor",methods=["POST"])
 def sensor():
+    abrirConexion()
 
     datos = request.json
     nombre = datos["nombre"]
     valor = datos["valor"]
 
     print (f"sensor: {nombre}, valor: {valor}")
-    
+    cerrarConexion()
+
     return "OK"
