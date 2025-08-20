6788fa5d9303c722bf911cbcc61f4ba8a7cd3e7d
SantiagoBurzolino <zemgogamer1@gmail.com>
2025-08-20 11:25:42 -0300

    modifique el codigo agregando un insert utilizando db.execute y db.commit. y despues cambie la respuesta que no sea ok sino que devuelva un tipo de estilo json devolviendo resultado ok usando jsonify

--- servidor/app.py
+++ servidor/app.py
@@ -3,22 +3,20 @@ from flask import Flask, g, jsonify, request, url_for
 from math import ceil
 
 def dict_factory(cursor, row):
- """Arma un diccionario con los valores de la fila."""
- fields = [column[0] for column in cursor.description]
- return {key: value for key, value in zip(fields, row)}
+    """Arma un diccionario con los valores de la fila."""
+    fields = [column[0] for column in cursor.description]
+    return {key: value for key, value in zip(fields, row)}
 
 def abrirConexion():
-  if 'db' not in g:
-     g.db = sqlite3.connect("SQL_Sensor.sqlite")
-     g.db.row_factory = dict_factory
-  return g.db
-
+    if 'db' not in g:
+        g.db = sqlite3.connect("SQL_Sensor.sqlite")
+        g.db.row_factory = dict_factory
+    return g.db
 
 def cerrarConexion(e=None):
-   db = g.pop('db', None)
-   if db is not None:
-       db.close()
-
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
 
 app = Flask(__name__)
 app.teardown_appcontext(cerrarConexion)
@@ -28,16 +26,65 @@ resultados_por_pag = 10
 def hello_world():
     return "<p>Hello, World!</p>"
 
-
-@app.route("/api/sensor",methods=["POST"])
+@app.route("/api/sensor", methods=["POST"])
 def sensor():
-    abrirConexion()
-
+    db = abrirConexion()  
     datos = request.json
     nombre = datos["nombre"]
     valor = datos["valor"]
+    
+   
+    db.execute("INSERT INTO valores (nombre, valor) VALUES (?, ?)", (nombre, valor))
+    
 
-    print (f"sensor: {nombre}, valor: {valor}")
-    cerrarConexion()
+    db.commit()
+    
+    cerrarConexion()  
 
-    return "OK"
+    return jsonify({'resultado': 'OK'})
+
+
+
+# import sqlite3
+# from flask import Flask, g, jsonify, request, url_for
+# from math import ceil
+
+# def dict_factory(cursor, row):
+#  """Arma un diccionario con los valores de la fila."""
+#  fields = [column[0] for column in cursor.description]
+#  return {key: value for key, value in zip(fields, row)}
+
+# def abrirConexion():
+#   if 'db' not in g:
+#      g.db = sqlite3.connect("SQL_Sensor.sqlite")
+#      g.db.row_factory = dict_factory
+#   return g.db
+
+
+# def cerrarConexion(e=None):
+#    db = g.pop('db', None)
+#    if db is not None:
+#        db.close()
+
+
+# app = Flask(__name__)
+# app.teardown_appcontext(cerrarConexion)
+# resultados_por_pag = 10
+
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
+
+# @app.route("/api/sensor",methods=["POST"])
+# def sensor():
+#     abrirConexion()
+
+#     datos = request.json
+#     nombre = datos["nombre"]
+#     valor = datos["valor"]
+
+#     print (f"sensor: {nombre}, valor: {valor}")
+#     cerrarConexion()
+
+#     return "OK"
